3ab2c70ee82abf36090bd4a6f13fdac123078b6d
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2022-05-30 13:22:40 +0530

    Positive or Negative number

---
+++ posneg.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter a number: ";
+	cin>>a;
+	if(a>=0)
+		cout<<a<<" is a positive number.\n";
+	else
+		cout<<a<<" is a negative number.\n";
+}
