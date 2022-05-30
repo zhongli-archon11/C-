a63bb5e86e06fcb09422ed0355e878fb1a3a087f
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2022-05-30 13:23:37 +0530

    Sum and Average of two numbers

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int no1,no2,sum;
+	float avg;
+	cout<<"Enter number: "<<endl;
+	cin>>no1;
+	cin>>no2;
+	sum= no1 + no2;
+	avg= sum/2;
+	cout<<"Sum= "<<sum<<endl;
+	cout<<"Average= "<<avg<<endl;
+	return 0;
+}
